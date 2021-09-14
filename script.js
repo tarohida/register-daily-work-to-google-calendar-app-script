49989a6d15ef2e6bd0b09b19a56937b8ef464166
taro-hida <sk8trou@gmail.com>
2021-09-14 23:07:54 +0900

    esa447 15分刻みで予定を作成する機能を実装

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 function run ()
 {
     const calendarId = 'primary';
-    const start = getRelativeDate(1, 12);
-    const end = getRelativeDate(1, 13);
-    const summary = 'Lunch Meeting';
-    createEvent(calendarId, start, end, summary)
+    const array = [
+        'title1',
+        'title2',
+        'title3'
+    ];
+    const start = new Date();
+    let end = new Date();
+    start.setHours(9,0,0);
+    array.forEach(summary => {
+            end.setTime(start.getTime() + 15 * 60 * 1000);
+            createEvent(calendarId, start, end, summary)
+            start.setTime(start.getTime() + 15 * 60 * 1000);
+        }
+    )
 }
 
 /**
@@ -24,21 +34,4 @@ function createEvent(calendarId, start, end, summary, description='') {
     };
     event = Calendar.Events.insert(event, calendarId);
     Logger.log('Event ID: ' + event.id);
-}
-
-/**
- * Helper function to get a new Date object relative to the current date.
- * @param {number} daysOffset The number of days in the future for the new date.
- * @param {number} hour The hour of the day for the new date, in the time zone
- *     of the script.
- * @return {Date} The new date.
- */
-function getRelativeDate(daysOffset, hour) {
-    var date = new Date();
-    date.setDate(date.getDate() + daysOffset);
-    date.setHours(hour);
-    date.setMinutes(0);
-    date.setSeconds(0);
-    date.setMilliseconds(0);
-    return date;
 }
